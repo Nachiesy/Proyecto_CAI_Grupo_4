9f1ec6a18806972bd6cbe03cd382b92038166be4
nacho <nacho@PCNACHO>
2023-10-03 22:07:02 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto_CAI_Grupo_4/Ingresar.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_CAI_Grupo_4
+{
+    public partial class Ingresar : Form
+    {
+        public Ingresar()
+        {
+            InitializeComponent();
+        }
+
+        private void ingresarbt(object sender, EventArgs e)
+        {
+
+        
+
+
+
+
+        }
+    }
+}
