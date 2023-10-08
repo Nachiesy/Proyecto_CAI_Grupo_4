5a183cd9e152bb7b85dd543d0eee196af6865c58
Facadavanzo <facudavanzo@gmail.com>
2023-10-08 18:23:55 -0300

    validar dni en presupuesto y fixes de modificaciones

--- Proyecto_CAI_Grupo_4/GenerarPresupuesto/GenerarPresupuestoHoteles.cs
+++ Proyecto_CAI_Grupo_4/GenerarPresupuesto/GenerarPresupuestoHoteles.cs
@@ -21,7 +21,7 @@ namespace Proyecto_CAI_Grupo_4
             datePickerFilterFechaHasta.Checked = false;
             datePickerFilterFechaHasta.Value = DateTime.Now.AddDays(1).Date;
 
-            AddProductosToDataGridViewProductos(GenerarPresupuestosManager.hoteles);
+            AddProductosToDataGridViewProductos(GenerarPresupuestosManager.hoteles.Where(x => x.Cantidad > 0));
 
             AddProductosSeleccionadosToDataGridView(GenerarPresupuestosManager.hotelesElegidos);
         }
@@ -47,7 +47,8 @@ namespace Proyecto_CAI_Grupo_4
                 var filter = new HotelesFilter(filterDto);
 
                 var productos = GenerarPresupuestosManager.hoteles
-                    .Where(x => (!filter.PrecioDesde.HasValue || x.Precio >= filter.PrecioDesde)
+                    .Where(x => x.Cantidad > 0
+                                && (!filter.PrecioDesde.HasValue || x.Precio >= filter.PrecioDesde)
                                 && (!filter.PrecioHasta.HasValue || x.Precio <= filter.PrecioHasta)
                                 && (!filter.FechaDesde.HasValue || x.FechaDesde == filter.FechaDesde)
                                 && (!filter.FechaHasta.HasValue || x.FechaHasta == filter.FechaHasta));
@@ -207,30 +208,37 @@ namespace Proyecto_CAI_Grupo_4
 
         private void btnConfirmarProductosSeleccionados_Click(object sender, EventArgs e)
         {
-            GenerarPresupuestosManager.hotelesElegidos.Clear();
-
-            foreach (DataGridViewRow row in dataGridViewProductosSeleccionados.Rows)
+            if (dataGridViewProductosSeleccionados.RowCount > 0)
             {
-                var idCellIndex = 0;
-                var cantidadSeleccionadaColumnIndex = 4;
-                var subTotalColumnIndex = 5;
+                GenerarPresupuestosManager.hotelesElegidos.Clear();
 
-                var id = Guid.Parse(row.Cells[idCellIndex].Value.ToString());
+                foreach (DataGridViewRow row in dataGridViewProductosSeleccionados.Rows)
+                {
+                    var idCellIndex = 0;
+                    var cantidadSeleccionadaColumnIndex = 4;
+                    var subTotalColumnIndex = 5;
 
-                var producto = GenerarPresupuestosManager.hoteles.Where(x => x.Id == id).SingleOrDefault();
+                    var id = Guid.Parse(row.Cells[idCellIndex].Value.ToString());
 
-                producto.CantidadSeleccionada = int.Parse(row.Cells[cantidadSeleccionadaColumnIndex].Value.ToString());
-                producto.SubTotal = decimal.Parse(row.Cells[subTotalColumnIndex].Value.ToString());
+                    var producto = GenerarPresupuestosManager.hoteles.Where(x => x.Id == id).SingleOrDefault();
 
-                GenerarPresupuestosManager.hotelesElegidos.Add(producto);
+                    producto.CantidadSeleccionada = int.Parse(row.Cells[cantidadSeleccionadaColumnIndex].Value.ToString());
+                    producto.SubTotal = decimal.Parse(row.Cells[subTotalColumnIndex].Value.ToString());
+
+                    GenerarPresupuestosManager.hotelesElegidos.Add(producto);
+                }
+
+                this.Close();
+
+                Thread thread = new Thread(OpenMenuGenerarPresupuesto);
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
             }
-
-            this.Close();
-
-            Thread thread = new Thread(OpenMenuGenerarPresupuesto);
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-        }
+            else
+            {
+                MessageBox.Show("Debes seleccionar Hoteles para poder confirmar el presupuesto");
+            }
+}
 
         private void btnVolverAlMenuGenerarPresupuestos_Click(object sender, EventArgs e)
         {
--- Proyecto_CAI_Grupo_4/GenerarPresupuesto/GenerarPresupuestoMenu.cs
+++ Proyecto_CAI_Grupo_4/GenerarPresupuesto/GenerarPresupuestoMenu.cs
@@ -37,7 +37,7 @@ namespace Proyecto_CAI_Grupo_4
             total += cruceros.Sum(x => x.SubTotal.Value);
             total += paquetesTuristicos.Sum(x => x.SubTotal.Value);
 
-            btnFinalizarPresupuesto.Enabled = total > 0 && !string.IsNullOrEmpty(textBoxClienteDNI.Text.Trim());
+            btnFinalizarPresupuesto.Enabled = total > 0 && ValidarDNI();
             presupuestoTotal.Text = total > 0 ? $"Total: {total:C2}" : "Total: $-";
         }
 
@@ -118,7 +118,7 @@ namespace Proyecto_CAI_Grupo_4
         {
             var dni = textBoxClienteDNI.Text.Trim();
 
-            if (productosElegidos.RowCount > 0 && !string.IsNullOrEmpty(dni))
+            if (productosElegidos.RowCount > 0 && ValidarDNI())
             {
                 var result = MessageBox.Show("¿Desea generar una Pre Reserva a partir de este Presupuesto?", string.Empty, MessageBoxButtons.YesNo);
 
@@ -131,12 +131,92 @@ namespace Proyecto_CAI_Grupo_4
                     MessageBox.Show($"Presupuesto generado correctamente para el cliente con DNI {dni}.", "Exito", MessageBoxButtons.OK);
                 }
 
+                ActualizarCantidadesDeProductos();
+
                 btnVolverMenuPrincipal_Click(sender, e);
             }
             else
             {
                 MessageBox.Show("Debes ingresar el DNI del Cliente a generarle el presupuesto y Debes elegir productos turisticos para poder generar un Presupuesto.", "Error", MessageBoxButtons.OK);
             }
+        }
+
+        private void ActualizarCantidadesDeProductos()
+        {
+            foreach (DataGridViewRow row in productosElegidos.Rows)
+            {
+                // ID
+                var idCellIndex = 0;
+                var id = Guid.Parse(row.Cells[idCellIndex].Value.ToString());
+
+                // Tipo de Servicio
+                var tipoDeServicioIndex = 2;
+                var tipoDeServicio = TipoDeServicioEnumHelper.GetTipoDeServicioEnum(row.Cells[tipoDeServicioIndex].Value.ToString());
+
+                // Cantidad Seleccionada
+                var cantidadSeleccionadaIndex = 4;
+                var cantidadSeleccionada = int.Parse(row.Cells[cantidadSeleccionadaIndex].Value.ToString());
+
+                switch (tipoDeServicio)
+                {
+                    case TipoDeServicioEnum.aereo:
+                        var indexAereo = GenerarPresupuestosManager.aereos.FindIndex(x => x.Id == id);
+                        var aereo = GenerarPresupuestosManager.aereos.SingleOrDefault(x => x.Id == id);
+
+                        aereo.Cantidad = aereo.Cantidad - cantidadSeleccionada;
+
+                        if (indexAereo != -1)
+                        {
+                            GenerarPresupuestosManager.aereos[indexAereo] = aereo;
+                        }
+
+                        break;
+
+                    case TipoDeServicioEnum.hotel:
+                        var indexHotel = GenerarPresupuestosManager.hoteles.FindIndex(x => x.Id == id);
+                        var hotel = GenerarPresupuestosManager.hoteles.SingleOrDefault(x => x.Id == id);
+
+                        hotel.Cantidad = hotel.Cantidad - cantidadSeleccionada;
+
+                        if (indexHotel != -1)
+                        {
+                            GenerarPresupuestosManager.hoteles[indexHotel] = hotel;
+                        }
+
+                        break;
+
+                    case TipoDeServicioEnum.crucero:
+                        var cruceroIndex = GenerarPresupuestosManager.cruceros.FindIndex(x => x.Id == id);
+                        var crucero = GenerarPresupuestosManager.cruceros.SingleOrDefault(x => x.Id == id);
+
+                        crucero.Cantidad = crucero.Cantidad - cantidadSeleccionada;
+
+                        if (cruceroIndex != -1)
+                        {
+                            GenerarPresupuestosManager.cruceros[cruceroIndex] = crucero;
+                        }
+
+                        break;
+
+                    case TipoDeServicioEnum.paquete:
+                        var paqueteIndex = GenerarPresupuestosManager.paquetesTuristicos.FindIndex(x => x.Id == id);
+                        var paquete = GenerarPresupuestosManager.paquetesTuristicos.SingleOrDefault(x => x.Id == id);
+
+                        paquete.Cantidad = paquete.Cantidad - cantidadSeleccionada;
+
+                        if (paqueteIndex != -1)
+                        {
+                            GenerarPresupuestosManager.paquetesTuristicos[paqueteIndex] = paquete;
+                        }
+
+                        break;
+                }
+            }
+
+            GenerarPresupuestosManager.aereosElegidos.Clear();
+            GenerarPresupuestosManager.hotelesElegidos.Clear();
+            GenerarPresupuestosManager.crucerosElegidos.Clear();
+            GenerarPresupuestosManager.paquetesTuristicosElegidos.Clear();
         }
 
         private void btnVolverMenuPrincipal_Click(object sender, EventArgs e)
@@ -167,12 +247,29 @@ namespace Proyecto_CAI_Grupo_4
                 }
             }
 
-            btnFinalizarPresupuesto.Enabled = total > 0 && !string.IsNullOrEmpty(textBoxClienteDNI.Text.Trim());
+            btnFinalizarPresupuesto.Enabled = total > 0 && ValidarDNI();
         }
 
-        private void productosElegidos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private bool ValidarDNI()
         {
+            var dni = textBoxClienteDNI.Text.Trim();
 
+            if (string.IsNullOrEmpty(dni))
+            {
+                return false;
+            }
+
+            if (!int.TryParse(dni, out int nroDeDoc))
+            {
+                return false;
+            }
+
+            if (nroDeDoc < 0 || nroDeDoc > 99999999)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
